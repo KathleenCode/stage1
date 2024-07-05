7e663e8a3473b3bb8cfaeb204f1d786d15256232
KathleenCode <kathleenbonaventure@gmail.com>
2024-07-05 08:41:36 +0000

    added vercel

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const express = require("express");
+import express from "express";
+
+import fetch from 'node-fetch';
+
+import bodyParser from "body-parser";
 
 const app = express();
+
+// app.use(express.json());
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.redirect("/api/hello");
 })
 
-app.get("/api/hello", (req, res) => {
+app.get("/api/hello", async (req, res) => {
     try {
-        // let ip = req.ip;
+        var fr = await fetch("https://ipapi.co/json/");
+        var fd = await fr.json();
         let visitor_name = req.query.visitor_name || "Mark";
         const greeting = {
         "client_ip": req.ip,
-        "location": "Abuja",
+        "location": fd.city,
         "greeting": `Hello ${visitor_name}!, the temperature is 11 degrees Celcius in Abuja`
     }
     res.json(greeting);
